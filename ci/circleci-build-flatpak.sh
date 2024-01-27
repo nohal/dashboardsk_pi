c2a48ea6576e2f2aef49c3efe8492103277e37e8
Pavel Kalian <pavel@kalian.cz>
2024-01-26 21:53:54 -0300

    Do not use the PPA repository for flatpak, not needed anymore

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -46,7 +46,7 @@ if [ -n "$CI" ]; then
         | sudo apt-key add -
 
     # Use updated flatpak (#457)
-    sudo add-apt-repository -y ppa:alexlarsson/flatpak
+    #sudo add-apt-repository -y ppa:alexlarsson/flatpak
     sudo apt update
 
     # Install or update flatpak and flatpak-builder
